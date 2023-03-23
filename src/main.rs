421b79ce8f83d0edfd79e737cf5433c32ca97ddc
partoftheworlD <partoftheworld@vivaldi.net>
2023-03-23 14:45:32 +0300

    Added exception filter

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use std::fmt::Error;
+
+use windows_sys::Win32::{Foundation::{HANDLE, EXCEPTION_SINGLE_STEP}, System::{Diagnostics::Debug::{EXCEPTION_POINTERS, CONTEXT}, Kernel::{ExceptionContinueExecution, ExceptionContinueSearch}}};
+
+struct Stack;
+trait Extractor {
+    fn new(&self) -> Self;
+    fn attach(&self, process_name: &str) -> Result<HANDLE, Error>;
+    fn set_breakpoint(&self, addr: u64);
+    unsafe extern "system" fn exception_filter(exception_info: *mut EXCEPTION_POINTERS) -> i32;
+}
+
+impl Extractor for Stack {
+    fn new(&self) -> Self {
+        Self
+    }
+
+    fn attach(&self, process_name: &str) -> Result<HANDLE, Error> {
+        todo!()
+    }
+
+    fn set_breakpoint(&self, addr: u64) {
+        todo!()
+    }
+
+    unsafe extern "system" fn exception_filter(exception_info: *mut EXCEPTION_POINTERS) -> i32 {
+        if (*(*exception_info).ExceptionRecord).ExceptionCode == EXCEPTION_SINGLE_STEP {
+            let debug_context= (*exception_info).ContextRecord;
+            todo!();
+            return ExceptionContinueExecution;
+        }
+        ExceptionContinueSearch
+    }
+}
+
+
+
 fn main() {
     println!("Hello, world!");
 }
