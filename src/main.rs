7449587d9eac3a46477d5935e51119607029b913
partoftheworlD <partoftheworld@vivaldi.net>
2023-03-23 21:35:57 +0300

    get process list

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-use std::fmt::Error;
+use std::{mem, ptr};
 
 use windows_sys::Win32::{
-    Foundation::{EXCEPTION_SINGLE_STEP, HANDLE},
+    Foundation::EXCEPTION_SINGLE_STEP,
     System::{
         Diagnostics::Debug::{SetUnhandledExceptionFilter, CONTEXT, EXCEPTION_POINTERS},
         Kernel::{ExceptionContinueExecution, ExceptionContinueSearch},
+        RemoteDesktop::WTSEnumerateProcessesA,
     },
 };
 const CONTEXT_DEBUG_REGISTERS: u32 = 0x10000 | 0x10;
@@ -12,8 +13,8 @@ const CONTEXT_DEBUG_REGISTERS: u32 = 0x10000 | 0x10;
 struct Stack;
 trait Extractor {
     fn new() -> Self;
-    fn attach(&self, process_name: &str) -> Result<HANDLE, Error>;
-    fn set_hw_breakpoint(&self, addr: u64, exception_filter: &fn(*const EXCEPTION_POINTERS));
+    unsafe fn attach(&self, process_name: &str);
+    fn set_hw_breakpoint(&self, addr: u64, exception_filter: fn(*const EXCEPTION_POINTERS));
     unsafe extern "system" fn exception_filter(exception_info: *const EXCEPTION_POINTERS) -> i32;
     unsafe extern "system" fn set_veh_breakpoint(&self, addr: u64);
 }
@@ -23,15 +24,37 @@ impl Extractor for Stack {
         Self
     }
 
-    fn attach(&self, process_name: &str) -> Result<HANDLE, Error> {
-        todo!()
+    unsafe fn attach(&self, process_name: &str) {
+        const WTS_CURRENT_SERVER_HANDLE: isize = 0;
+        let mut process_info = ptr::null_mut();
+        let mut process_count = 0u32;
+        println!("Start WTSEnumerateProcessesA");
+        let wts_result = WTSEnumerateProcessesA(
+            WTS_CURRENT_SERVER_HANDLE,
+            0u32,
+            1,
+            &mut process_info,
+            &mut process_count,
+        );
+        if wts_result == 0 {
+            panic!("WTSEnumerateProcessesA failed");
+        } else {
+            for idx in 0..=process_count {
+                let process_name = (*(process_info).offset(idx as isize)).pProcessName;
+                if process_name.is_null() {
+                    println!("Found process: {process_name:?}");
+                }
+                // println!("{:?}", std::str::from_utf8_mut((process_info).offset(idx as isize)).pProcessName);
+            }
+            println!("{process_count:?}");
+        }
     }
 
-    fn set_hw_breakpoint(&self, addr: u64, exception_filter: &fn(*const EXCEPTION_POINTERS)) {
+    fn set_hw_breakpoint(&self, addr: u64, exception_filter: fn(*const EXCEPTION_POINTERS)) {
         unsafe {
             SetUnhandledExceptionFilter(Some(std::mem::transmute(exception_filter)));
         }
-        let mut thread_context: CONTEXT = unsafe { std::mem::zeroed() };
+        let mut thread_context: CONTEXT = unsafe { mem::zeroed() };
         thread_context.ContextFlags = CONTEXT_DEBUG_REGISTERS;
         thread_context.Dr0 = addr;
         thread_context.Dr7 = 1 << 0;
@@ -51,5 +74,8 @@ impl Extractor for Stack {
 }
 
 fn main() {
-    let mut stack = Stack::new();
+    let stack = Stack::new();
+    unsafe {
+        stack.attach("notepad.exe");
+    }
 }
