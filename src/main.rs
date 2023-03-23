2c338482589fff29dc4460385250a623a02717c1
partoftheworlD <partoftheworld@vivaldi.net>
2023-03-23 22:52:54 +0300

    from_lpstr

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,19 @@ trait Extractor {
     unsafe extern "system" fn set_veh_breakpoint(&self, addr: u64);
 }
 
+trait Decoder {
+    fn from_lpstr(&self, string: *mut u8) -> String;
+}
+
+impl<T: Extractor> Decoder for T{
+    // https://github.com/Traverse-Research/hassle-rs/blob/ddcdfc6032657b2b8d75d1bc55719d186ad7e55e/src/utils.rs#L34
+    fn from_lpstr(&self, string: *mut u8) -> String {
+        let len = (0..).take_while(|&i| unsafe { *string.offset(i) } != 0).count();
+        let slice: &[u8] = unsafe { std::slice::from_raw_parts(string.cast(), len) };
+        std::str::from_utf8(slice).map(|s| s.to_owned()).unwrap()
+    }
+}
+
 impl Extractor for Stack {
     fn new() -> Self {
         Self
@@ -37,9 +50,9 @@ impl Extractor for Stack {
             &mut process_count,
         );
         if wts_result != 0 {
-            for idx in 0..=process_count {
-                let x = (*process_info.offset((idx).try_into().unwrap())).pProcessName;
-                println!("{:?}", Some(x));
+            for idx in 0..process_count {
+                let process_name = self.from_lpstr((*process_info.offset((idx).try_into().unwrap())).pProcessName);
+                println!("{:?}", process_name);
             }
             println!("{process_count:?}");
         } else {
