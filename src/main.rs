6bdb890704250cda5240152d10f934fb8704e8c6
partoftheworlD <partoftheworld@vivaldi.net>
2023-03-23 23:36:39 +0300

    Adding unsafe blocks Adding unsafe blocks instead of extra unsafe definition

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ const CONTEXT_DEBUG_REGISTERS: u32 = 0x10000 | 0x10;
 struct Stack;
 trait Extractor {
     fn new() -> Self;
-    unsafe fn attach(&self, process_name: &str);
+    fn attach(&self, process_name: &str);
     fn set_hw_breakpoint(&self, addr: u64, exception_filter: fn(*const EXCEPTION_POINTERS));
     unsafe extern "system" fn exception_filter(exception_info: *const EXCEPTION_POINTERS) -> i32;
     unsafe extern "system" fn set_veh_breakpoint(&self, addr: u64);
@@ -39,21 +39,21 @@ impl Extractor for Stack {
         Self
     }
 
-    unsafe fn attach(&self, process_name: &str) {
+    fn attach(&self, process_name: &str) {
         const WTS_CURRENT_SERVER_HANDLE: isize = 0;
         let mut process_info = ptr::null_mut();
         let mut process_count = 0u32;
-        let wts_result = WTSEnumerateProcessesA(
+        let wts_result = unsafe { WTSEnumerateProcessesA(
             WTS_CURRENT_SERVER_HANDLE,
             0u32,
             1,
             &mut process_info,
             &mut process_count,
-        );
+        ) };
         if wts_result != 0 {
             for idx in 1..process_count {
                 let process_name = Stack::from_lpstr(
-                    (*process_info.offset((idx).try_into().unwrap())).pProcessName,
+                    (unsafe { *process_info.offset((idx).try_into().unwrap()) }).pProcessName,
                 );
                 println!("{process_name:?}");
             }
@@ -88,7 +88,5 @@ impl Extractor for Stack {
 
 fn main() {
     let stack = Stack::new();
-    unsafe {
-        stack.attach("notepad.exe");
-    }
+    stack.attach("notepad.exe");
 }
