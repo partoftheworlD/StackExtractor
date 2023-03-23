63e9e3f5c2d003d270bb0bf8e055329ccf7a6d47
partoftheworlD <partoftheworld@vivaldi.net>
2023-03-23 14:47:06 +0300

    remove &self in new

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,14 @@ use windows_sys::Win32::{Foundation::{HANDLE, EXCEPTION_SINGLE_STEP}, System::{D
 
 struct Stack;
 trait Extractor {
-    fn new(&self) -> Self;
+    fn new() -> Self;
     fn attach(&self, process_name: &str) -> Result<HANDLE, Error>;
     fn set_breakpoint(&self, addr: u64);
     unsafe extern "system" fn exception_filter(exception_info: *mut EXCEPTION_POINTERS) -> i32;
 }
 
 impl Extractor for Stack {
-    fn new(&self) -> Self {
+    fn new() -> Self {
         Self
     }
 
